0b1b93e572816b22e2a0d8a347d4ec323243e1cf
Lam Nguyen <me@madebylam.com>
2013-03-28 00:52:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footballer::Application.config.secret_token = 'c25c5ad1b9b66fc33efe8414cacbad9b7a9397c4e1dfe4e7489833cbfebf5ab1f3f59be8fd10ee09fefa651e675106f6e3848d762a3e6bfdaedde2dd9e642612'
