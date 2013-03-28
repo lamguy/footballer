45a2436317ccbfdd7724e3cb769bfd114a025b0a
Lam Nguyen <me@madebylam.com>
2013-03-28 04:38:59 -0500

    added Facebook columns to Users with migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328063351) do
+ActiveRecord::Schema.define(:version => 20130328093643) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(:version => 20130328063351) do
     t.string   "state"
     t.string   "postal"
     t.string   "country"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
