354d5b082c9b7c357b52fe6bcc0aced9db8cfe93
Lam Nguyen <me@madebylam.com>
2013-03-29 02:48:25 -0500

    defined relationship for post, comment and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :content, :summary, :tags, :title, :type
+
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
 
   has_many :posts
+  has_many :comments
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
 	  user = User.where(:provider => auth.provider, :uid => auth.uid).first
