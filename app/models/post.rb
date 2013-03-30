1c292b4d62097ae58cb356ff959f82772d03822f
Lam Nguyen <me@madebylam.com>
2013-03-30 02:18:44 -0500

    use existed redactor to retrieve image associated with post by asset id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :content, :summary, :tags, :title, :posttype, :thumbnail, :header_image
+  attr_accessible :content, :summary, :tags, :title, :posttype, :thumbnail, :header_image, :bigimage
   has_many :comments
+
+  def bigimage
+  	@picture = RedactorRails.picture_model.where(:id => header_image).first
+  	@picture.image
+  end
+
 end
