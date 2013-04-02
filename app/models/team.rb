7480fdfb00b4293e62e72c2a8b0851bd0d46f086
Lam Nguyen <me@madebylam.com>
2013-04-02 09:18:02 -0500

    added shortname for teams to use in some cases

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
   belongs_to :division
   has_many :players, :through => :playing_team
-  attr_accessible :history, :logo, :name, :division_id
+  attr_accessible :history, :logo, :name, :division_id, :shortname
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402090628) do
+ActiveRecord::Schema.define(:version => 20130402141545) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -239,6 +239,7 @@ ActiveRecord::Schema.define(:version => 20130402090628) do
     t.integer  "division_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "shortname"
   end
 
   add_index "teams", ["division_id"], :name => "index_teams_on_division_id"
