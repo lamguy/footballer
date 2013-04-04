e236cc925a1ec99fb165adce6a4aa327622ec7cb
Lam Nguyen <me@madebylam.com>
2013-04-03 23:14:44 -0500

    no longer using datetimepicker gem then we we removed the js implemented before

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,15 +1,3 @@
 //= require jquery
 //= require jquery.ui.all
 //= require active_admin/base
-$(document).ready(function() {
-  $('input.hasDatetimePicker').datetimepicker({
-    dateFormat: "dd/mm/yy",
-    beforeShow: function () {
-      setTimeout(
-        function () {
-          $('#ui-datepicker-div').css("z-index", "3000");
-        }, 100
-      );
-    }
-  });
-});
