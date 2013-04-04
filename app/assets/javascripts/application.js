c19eeb8fc084eceed2e136a7075c3d1669bb34a5
Lam Nguyen <me@madebylam.com>
2013-04-04 07:12:41 -0500

    use jquery tabs ui to separate live commentary panel and news panel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,14 @@ $(document).ready(function() {
 		$(this).parents('.has-dropdown').removeClass('hover');
 	});
 
+	$( "#match-tasks" ).tabs({
+      beforeLoad: function( event, ui ) {
+        ui.jqXHR.error(function() {
+          ui.panel.html("Tab này đang được hoàn thiện, sẽ hoạt động trong thời gian ngắn nhất có thể.");
+        });
+      }
+    });
+
 	$('#live-commentary .commentary-inner').jScrollPane();
 })
 
